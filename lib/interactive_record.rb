4b4ef08cdb6a960d3264b77b56fc0a532229330b
rheadp <rheadpea@msu.edu>
2021-03-10 12:46:13 -0500

    commit tree 9c2ba944138787cd024b33761a7b596697f2c0ce

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,47 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+
+    def self.table_name
+      self.to_s.downcase.pluralize
+    end
+    
+    def self.column_names
+      DB[:conn].results_as_hash = true
+      DB[:conn]
+      .execute("PRAGMA table_info('#{table_name}')")
+      .map {|info| info['name']}
+    end
   
-end
+    def self.find_by_name(name)
+      DB[:conn].results_as_hash = true
+      DB[:conn].execute("SELECT * FROM #{table_name} WHERE name = ?", name)
+    end
+  
+    def self.find_by(hash)
+      DB[:conn].results_as_hash = true
+      condition = hash.map{|k,v| "#{k} = '#{v}'"}[0]
+      DB[:conn].execute("SELECT * FROM #{table_name} WHERE #{condition}")    
+    end
+  
+    def table_name_for_insert
+      self.class.table_name
+    end
+  
+    def col_names_for_insert
+      self.class.column_names.select{|s| s != "id"}.join(", ")
+    end
+  
+    def values_for_insert
+      self.class.column_names.map {|name| 
+        value = self.send(name.to_s)
+        "'#{value}'" if value
+      }.compact.join(", ")
+    end
+  
+    def save
+      DB[:conn].execute("INSERT INTO #{table_name_for_insert}(#{col_names_for_insert}) VALUES(#{values_for_insert})" )
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end
+  end
--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,10 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+    def initialize(attr_hash = {})
+    self.class.column_names.each {|col_name|
+      self.class.attr_accessor col_name.to_sym
+    }
+    attr_hash.each {|k, v| self.send("#{k}=", v)}
+end
 end
